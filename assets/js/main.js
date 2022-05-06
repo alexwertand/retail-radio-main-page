7bc31ed4a6ad926c2951708ff5a1badf0c061db7
wert_and <alexandrphone07@gmail.com>
2022-05-06 17:44:33 +0300

    added mobile-menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,12 @@
-let hamburger = document.querySelector('.hamburger');
+let hamburger = document.querySelector('.hamburger'),
+    bodyEl = document.getElementsByTagName('body')[0],
+    moibileMenuEl = document.querySelector('.mobile-menu');
+
+console.log(bodyEl);
 
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('is-active');
+    bodyEl.classList.toggle('mobile-menu-open');
+    bodyEl.classList.toggle('overflow-hidden');
+    moibileMenuEl.classList.toggle('d-none');
 });
